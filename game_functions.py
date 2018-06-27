0ea9224c2a66a6f19b9c9136bc2a0e5ec8da672b
Sarah <segrissom@randolphcollege.edu>
2018-06-27 16:15:53 -0500

    Refactoring check events and breaking it up
    Added two new defs to make check_events slimmer and less bulky.

--- game_functions.py
+++ game_functions.py
@@ -1,27 +1,34 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """This responds to kepresses"""
+    if event.key == pygame.K_RIGHT:
+        # this will move the ship over while the right key is held
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        # this will move the ship over left while the left key is pressed
+        ship.moving_left = True
+def check_keyup_events(event, ship):
+    """This responds to the keyups"""
+    if event.key == pygame.K_RIGHT:
+        # This will stop moving the ship to the right once you release up on the right
+        # key
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        # this will stop moving the ship to the left
+        # one the key is released
+        ship.moving_left = False
+
 def check_events():
     """Responds to key presses and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # this will move the ship over while the right key is held
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                # this will move the ship over left while the left key is pressed
-                ship.moving_left = True
+            check_keyup_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                # This will stop moving the ship to the right once you release up on the right
-                # key
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                # this will stop moving the ship to the left
-                # one the key is released
-                ship.moving_left = False
+            check_keydown_events(event, ship)
 
 
 
