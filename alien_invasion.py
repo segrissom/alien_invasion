644d39d83ee82e45e3caaf94a213e92bc81666d6
Sarah <segrissom@randolphcollege.edu>
2018-06-27 16:16:41 -0500

    Updating to keep current with changes
    Updated so now it's current with the other classes, functions, and all

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
+import game_functions as gf
 
 def run_game():
     # Initialize game and create a screen object
@@ -11,23 +10,16 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Setting the background color
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game
     while True:
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # Redraw the screen during each pass through the loop
-        screen.fill(bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
